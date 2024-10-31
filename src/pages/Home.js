1080d4288ac2b73a6291ed603769e1295de6f2e3
erick-hualla-code <erick.hualla.code@gmail.com>
2024-10-31 15:09:07 -0500

    Fix: Merged changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import { Hidden, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import DisplacementSphere from '../components/background/DisplacementSphere';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
-import { FooterText } from '../components/footer/FooterText';
 import { SocialIcons } from '../components/content/SocialIcons';
 import { SpeedDials } from '../components/speedDial/SpeedDial';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
@@ -64,7 +63,6 @@ export const Home = () => {
         <Hidden mdUp>
           <SpeedDials />
         </Hidden>
-        <FooterText />
         <Button className={classes.arrowButton} onClick={handleRedirect}> {/* Añadir onClick */}
           <ArrowDownwardIcon />
         </Button>
